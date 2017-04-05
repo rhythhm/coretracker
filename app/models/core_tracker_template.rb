14a1c4f8e53bd3c2d6bdcf73e7bd26193d25f488
aakashaggarwal <aakashaggarwal.1993@gmail.com>
2017-04-06 02:59:42 +0530

    fixed

--- app/models/core_tracker_template.rb
+++ app/models/core_tracker_template.rb
@@ -5,18 +5,18 @@ class CoreTrackerTemplate < ActiveRecord::Base
   end
 
   def soh_by_roh
-    if last_180days_saleqty == 0
+    if self.last_180days_saleqty == 0
      return 0
    else
-     (soh / last_180days_saleqty * 180)
+     (self.soh / self.last_180days_saleqty * 180)
    end
   end
 
   def soh_oo_roh
-    if last_180days_saleqty == 0
+    if self.last_180days_saleqty == 0
      return 0
    else
-     (soh + open_po ) / last_180days_saleqty * 180
+     (self.soh + self.open_po ) / self.last_180days_saleqty * 180
    end
   end
 
