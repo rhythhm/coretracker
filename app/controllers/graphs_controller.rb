fe2ce03a6ca92d3861b536710716a14b4e6c64e1
aakashaggarwal <aakashaggarwal.1993@gmail.com>
2017-04-16 14:09:30 +0530

    Table

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,4 +1,8 @@
 class GraphsController < ApplicationController
   def index
+    @data = [
+      {name: "Current Year Sales", data: CoreTrackerTemplate.group(:style).sum(:cy_saleqty)},
+      {name: "Previous Year Sales", data: CoreTrackerTemplate.group(:style).sum(:py_saleqty)}
+    ]
   end
 end
--- app/controllers/pivot_controller.rb
+++ app/controllers/pivot_controller.rb
@@ -1,5 +1,46 @@
 class PivotController < ApplicationController
   def index
+    @table = {}
+    if params[:group_name]
+      table = "group_name, "
+    else
+      table = " "
+    end
+
+    if params[:brand_name]
+      table = table + "brand_name, "
+    else
+      table = table + " "
+    end
+
+    if params[:store_code]
+      table = table + "store_code, "
+    else
+      table = table + " "
+    end
+
+    if params[:item_code]
+      table = table + "item_code, "
+    else
+      table = table + " "
+    end
+
+    if params[:style]
+      table = table + "style, "
+    else
+      table = table + " "
+    end
+
+    table = table.strip
+
+    if table.empty?
+      @default = 'on'
+    else
+      @default = 'off'
+
+      table = table[0..-2]
+    end
+
     @core_tracker_templates = CoreTrackerTemplate.find(:all,
                        :select => "brand_name, group_name,
                        sum(core_tracker_templates.cy_saleqty) AS sum_cy_saleqty,
@@ -8,7 +49,7 @@ class PivotController < ApplicationController
                        GROUP_CONCAT(last_180days_saleqty, ',') AS single_last_180days_saleqty,
                        GROUP_CONCAT(soh, ',') AS single_soh,
                        GROUP_CONCAT(open_po, ',') AS single_open_po",
-                       :group => "brand_name, group_name")
+                       :group => "#{table}")
   end
 end
 
